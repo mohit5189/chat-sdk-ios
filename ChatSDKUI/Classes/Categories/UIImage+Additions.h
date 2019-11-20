791374acff191f4a414ac2c3082a2f84335758a3
Mohit Kumar <mohit.kumar@lalamove.com>
2019-11-20 11:47:41 +0530

    fix warnings for header file

--- ChatSDKUI/Classes/Categories/UIImage+Additions.h
+++ ChatSDKUI/Classes/Categories/UIImage+Additions.h
@@ -6,15 +6,6 @@
 //  Copyright (c) 2013 Benjamin Smiley-andrews. All rights reserved.
 //
 
-@interface UIImage (Additions)
-
--(NSString *) saveImageToFileWithName: (NSString *) name withFolder: (NSString *) folder;
-+(UIImage *) getImageFromFile: (NSString *) name withFolder: (NSString *) folder;
-+(NSString *) getPathForFile: (NSString *) name withFolder: (NSString *) folder;
-+(UIImage *) getImageForPath: (NSString *) path;
-
-@end
-
 @implementation UIImage (Additions)
 
 -(NSString *) saveImageToFileWithName: (NSString *) name withFolder: (NSString *) folder {
